13499c46a0ae6f6cbf8c7174b05e9143ffd113aa
ranto <rantoirwanda08@gmail.com>
2024-02-10 18:51:02 +0700

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@/src/app/ , @/src/components/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
